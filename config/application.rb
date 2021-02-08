9ddb33b88e682307416e0e661eddb15f90e42447
motoki6318 <mksk6318@icloud.com>
2021-02-08 11:21:30 +0900

    first commit

---
+++ config/application.rb
@@ -0,0 +1,31 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module ChatApp
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
+
+    config.gecerators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
+    # 生成したファイルに対応したスタイルシート
+    # 生成したファイルに対応したヘルパー
+    # 生成したファイルに対応したJavaScriptファイル
+    # アプリケーションをテストするためのファイル
+    # コントローラー生成時
+    # 要らないファイルを作らせない記述
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+  end
+end
