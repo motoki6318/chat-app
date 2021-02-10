77dc2352c9179c582d7b7804cd06248e59c9cb11
motoki6318 <mksk6318@icloud.com>
2021-02-10 11:55:41 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,28 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
+    # binding.pry
+    # @roomにここでidを渡すことでform_withで役立つ
   end
+
+  def create
+    @room = Room.find(params[:room_id])
+    # 先に親であるroomのidを取得
+    @message = @room.messages.new(message_params)
+    # メッセージを作る時に@roomで　どのルームかを明記
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
+    # merge(user_id: current_user.id)でコメントした本人を限定する
+  end
+
 end
